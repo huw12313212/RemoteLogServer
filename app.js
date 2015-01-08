616980dff5f6154badc301ac924bf9867eafc6c2
王瀚宇 <huw12313212@gmail.com>
2015-01-08 15:19:34 +0800

    done

--- app.js
+++ app.js
@@ -1,19 +1,48 @@
 var express = require('express')
 	, app = express()
-  	, server = require('http').createServer(app);
+  	, server = require('http').createServer(app)
+    , io = require('socket.io').listen(server,{ log: false });
 
 var port = process.env.PORT||5566;
+var monitorPort = 5567;
 //var net = require('net');
 var Logger = require('./Logger.js');
 var ws = require("nodejs-websocket");
 
-// Start a TCP Server
+
+server.listen(monitorPort);
+
+app.use(express.static(__dirname+"/client/"));
+
+app.use(express.favicon());
+app.get('/', function (req, res) {
+  res.sendfile(__dirname + '/client/index.html');
+});
+
+// LoggerServer
 var server = ws.createServer(function (conn) {
     Logger.log("New connection")
     conn.on("text", function (str) {
 
-        //console.log(conn);
-        Logger.log(str)
+        var array = str.split(/[ ,\":]/);
+
+        var data = {};
+        data.hour = parseFloat(array[1]);
+        data.min = parseFloat(array[2]);
+        data.sec = parseFloat(array[3].split('.')[0]);
+        data.millisecond =  (parseFloat(array[3])*1000)%1000;
+        data.P = parseFloat(array[25]);
+        data.I = parseFloat(array[31]);
+        data.D = parseFloat(array[37]);
+        data.yaw = parseFloat(array[7]);
+        data.pitch = parseFloat(array[13]);
+        data.roll = parseFloat(array[19]);
+
+        io.emit('data', data);
+        Logger.log(JSON.stringify(data));
+
+
+
         //conn.sendText(str.toUpperCase()+"!!!")
     })
     conn.on("close", function (code, reason) {
@@ -21,4 +50,17 @@ var server = ws.createServer(function (conn) {
     })
 }).listen(port);
  
-Logger.log("Log Server Running web socket at port " + port);
+Logger.log("Log Server Running web socket at port " + port); 
+Logger.log("Log Monitor Running web socket at port " + monitorPort);
+
+
+io.sockets.on('connection', function (socket) {
+    //show all clients
+    console.log(socket.id + "connected");
+
+    //disconnect
+    socket.on('disconnect',function(){
+        console.log(socket.id+' disconnect!');
+    });
+
+});
